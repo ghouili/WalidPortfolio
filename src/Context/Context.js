ba960eaca690fb422a2e3126831f81a9def19dab
ghouili <walid.ghouili98@gmail.com>
2023-02-03 21:56:39 +0100

    commit V1.0.2

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,18 +1,15 @@
-import {createContext, useReducer} from 'react';
+import { createContext, useReducer } from 'react';
 
 export const themeContext = createContext();
 
-const initialState = {darkMode: false};
+const initialState = { darkMode: false };
 
 const themeReducer = (state, action) => {
     switch (action.type) {
         case 'toggle':
-            return {darkMode: !state.darkMode};
-            break;
-    
+            return { darkMode: !state.darkMode };
         default:
             return state;
-            break;
     }
 };
 
@@ -20,7 +17,7 @@ export const ThemeProvider = (props) => {
     const [state, dispatch] = useReducer(themeReducer, initialState);
     return (
         <themeContext.Provider
-            value={{ state, dispatch}}
+            value={{ state, dispatch }}
         >
             {props.children}
         </themeContext.Provider>
